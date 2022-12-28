eef693a31dfbbd1990a33bd34afe9e2123be810b
reski-id <pesan.reski@gmail.com>
2022-12-29 00:13:00 +0700

    Simple Component in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TodoRowsItem from './components/TodoRowsItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
+        Your Todo's
+        </div>
+        <div className='card-body'>
+          <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th scope='col'>#</th>
+              <th scope='col'>Description</th>
+              <th scope='col'>Asign</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TodoRowsItem/>
+            <tr>
+              <th scope='row'>2</th>
+              <td>Get Hair Cut</td>
+              <td>Eric</td>
+            </tr>
+          </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoRowsItem.js
@@ -0,0 +1,11 @@
+function TodoRowsItem() {
+    return (
+        <tr>
+            <th scope="row">1</th>
+            <td>Feed Dog</td>
+            <td>Eric</td>
+        </tr>
+    )
+}
+
+export default TodoRowsItem
