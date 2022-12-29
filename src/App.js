ac4f1892883d322fc7144f1108eaf7efb9634096
reski-id <pesan.reski@gmail.com>
2022-12-29 11:15:42 +0700

    use State; button add newTodo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React, {useState} from 'react';
 import './App.css';
 import TodoTables from './components/TodoTables';
 
 function App() {
-  const todos  = [
-    {RowsNumber : 1, RowsItems: "Feed Cat", RowsAssigned : "User One"}, 
-    {RowsNumber : 2, RowsItems: "Get Haircut", RowsAssigned : "Thor"}, 
-    {RowsNumber : 3, RowsItems: "Water Plants", RowsAssigned : "User One"}, 
-    {RowsNumber : 4, RowsItems: "Charge Phone Battery", RowsAssigned : "Thor"}, 
-  ]
+  const [todos, setTodos]  =  useState(
+    [
+      {RowsNumber : 1, RowsItems: "Feed Cat", RowsAssigned : "User One"}, 
+      {RowsNumber : 2, RowsItems: "Get Haircut", RowsAssigned : "Thor"}, 
+      {RowsNumber : 3, RowsItems: "Water Plants", RowsAssigned : "User One"}, 
+      {RowsNumber : 4, RowsItems: "Charge Phone Battery", RowsAssigned : "Thor"}, 
+    ]
+  
+  )
+  const addTodo = () =>{
+    if (todos.length>0){
+      const newTodo = {
+        RowsNumber : todos.length+1, RowsItems: "Think todo", RowsAssigned : "From Click Event"
+      }
+      setTodos(todos => [...todos, newTodo]);
+    }
+  }
   return (
     <div className='mt-5 container'>
       <div className='card'>
@@ -16,6 +28,10 @@ function App() {
         </div>
         <div className='card-body'>
           <TodoTables todos={todos}/>
+          <button className='btn btn-primary' onClick={
+            addTodo
+          }>
+            Add New Todo</button>
         </div>
       </div>
     </div>
